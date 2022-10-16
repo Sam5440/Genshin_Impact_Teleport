9a0c28b5557c04161dfdc774990bceeb92a8de64
sam <1102566608@qq.com>
2022-10-16 22:53:15 +0800

    fix_ID1/2提瓦特?

--- OtherFile/raw_data/Version Now/json_export.py
+++ OtherFile/raw_data/Version Now/json_export.py
@@ -1,53 +1,69 @@
-item_json_name = "GadgetSpawns.json"
-
-import json
+imrt json
 import os
 import re
-from pydoc import describe
 from unittest import result
 
 import matplotlib.pyplot as plt
 
+###########
+language_name = ["zh-cn", "zh-tw", "en-us", "ru-ru"]
+item_json_name = "GadgetSpawns.json"
+monster_json_name = "Spawns.json"
+###########
+############################################
+def road_long(point1, point2):
+    return (
+        (point1[0] - point2[0]) ** 2
+        + (point1[1] - point2[1]) ** 2
+        + (point1[2] - point2[2]) ** 2
+    ) ** 0.5
 
-def road_long(point1,point2):
-    return ((point1[0]-point2[0])**2+(point1[1]-point2[1])**2+(point1[2]-point2[2])**2)**0.5
 
 def nearest_road(points):
     if len(points) <= 1:
         return points
     result = [points[0]]
-    while points!=[]:
+    while points != []:
         min = 2147483647
         point = result[-1]
-        for index,next_point in enumerate(points):
-            if road_long(next_point,point)<min:
-                min = road_long(next_point,point)
-                index_nearest = index #points.index(point)
+        for index, next_point in enumerate(points):
+            if road_long(next_point, point) < min:
+                min = road_long(next_point, point)
+                index_nearest = index  # points.index(point)
         result.append(points[index_nearest])
         points.pop(index_nearest)
     # print(result)
     return result
 
+
 def count_road_long(points):
-    #计算路径长度
+    # 计算路径长度
     long = 0
-    for i in range(len(points)-1):
-        long += ((points[i][0]-points[i+1][0])**2+(points[i][1]-points[i+1][1])**2+(points[i][2]-points[i+1][2])**2)**0.5
+    for i in range(len(points) - 1):
+        long += (
+            (points[i][0] - points[i + 1][0]) ** 2
+            + (points[i][1] - points[i + 1][1]) ** 2
+            + (points[i][2] - points[i + 1][2]) ** 2
+        ) ** 0.5
     return long
-def draw_3d(points,show=False):
+
+
+def draw_3d(points, show=False):
     print(count_road_long(points))
     x = [i[0] for i in points]
     y = [i[1] for i in points]
     z = [i[2] for i in points]
     if show:
         fig = plt.figure()
-        ax = fig.add_subplot(111, projection='3d')
+        ax = fig.add_subplot(111, projection="3d")
         ax.plot(x, y, z)
-        ax.set_xlabel('X Label')
-        ax.set_ylabel('Y Label')
-        ax.set_zlabel('Z Label')
+        ax.set_xlabel("X Label")
+        ax.set_ylabel("Y Label")
+        ax.set_zlabel("Z Label")
         plt.show()
 
+
+##############################################################
 def path_fix(path):
     path = path.replace('"', "'")
     return path
@@ -58,7 +74,10 @@ def new_folder(path):
     if not os.path.exists(path):
         os.makedirs(path)
 
+
 count_dict = {}
+
+
 def count(key="N"):
     global count_dict
     if key in count_dict:
@@ -68,11 +87,7 @@ def count(key="N"):
 
 
 # load id_json 文件夹内全部文件
-def item_point_generate(language_id):
-    global count_dict
-    # language_id = 3  # 0:cn,1:tw,2:en,3:ru
-    language_name = ["zh-cn", "zh-tw", "en-us", "ru-ru"]
-    folader_name = os.path.join(os.path.dirname(__file__), "ID_json")
+def get_all_id_name(folader_name):
     id_json = {}
     for file in os.listdir(folader_name):
         print(file)
@@ -80,55 +95,16 @@ def item_point_generate(language_id):
             # print(json.load(f))
             # file_json = {file:json.load(f)}
             id_json[file] = json.load(f)
-    item_json_path = os.path.join(os.path.dirname(__file__), "pos", item_json_name)
-    with open(item_json_path, "r") as f:
-        item_json = json.load(f)
-
-    result_path = os.path.join(
-        os.path.dirname(__file__), "result", language_name[language_id]
-    )
-    if not os.path.exists(os.path.dirname(result_path)):
-        os.makedirs(os.path.dirname(result_path))
-
-    item_dict = {}
+    return id_json
 
 
-    for scene_data in item_json:
-        # print(scene_data["sceneId"])
-        scene_data["sceneId"] = str(scene_data["sceneId"])
-        scene_data["sceneId"] = id_json["Scene.json"][scene_data["sceneId"]][language_id]
-        if scene_data["sceneId"] not in item_dict.keys():
-            item_dict.update({scene_data["sceneId"]: {}})
-        new_folder(os.path.join(result_path, scene_data["sceneId"]))
-        for item in scene_data["spawns"]:
-            if "gatherItemId" in item:
-                item["gatherItemId"] = str(item["gatherItemId"])
-                if item["gatherItemId"] in id_json["Item.json"].keys():
-                    item["gatherItemId"] = id_json["Item.json"][item["gatherItemId"]][
-                        language_id
-                    ]
-                if item["gatherItemId"] not in item_dict[scene_data["sceneId"]].keys():
-                    item_dict[scene_data["sceneId"]].update({item["gatherItemId"]: []})
-                new_folder(
-                    os.path.join(result_path, scene_data["sceneId"], item["gatherItemId"])
-                )
-                pos = [item["pos"]["x"], item["pos"]["y"], item["pos"]["z"]]
-                item_dict[scene_data["sceneId"]][item["gatherItemId"]].append(pos)
-                count(item["gatherItemId"])
-        # count(scene_data["sceneId"])
-    # print(item_dict)
-    count_dict_sort = sorted(count_dict.items(), key=lambda x: x[1], reverse=True)  # 按照value排序
-    print(count_dict_sort)
-    count_dict = {}
-
-
-
+def save_json(result_path, item_dict):
     pos_json = {}  # formate
     for scene, items in item_dict.items():
         print(scene)
         for item, points in items.items():
             i = 0
-            if len(points) >100:
+            if len(points) > 100:
                 print(f"{len(points)}{item}-{scene}")
                 draw_3d(points)
                 best_road = nearest_road(points)
@@ -138,11 +114,13 @@ def item_point_generate(language_id):
             for pos in best_road:
                 i += 1
                 pos_name = f"{i}-{item}-{scene}"
-                pos_json["description"] = str(i)#拼音
-                pos_json["name"] = str(i) #拼音
+                pos_json["description"] = str(i)  # 拼音
+                pos_json["name"] = str(i)  # 拼音
                 pos_json["position"] = pos
                 with open(
-                    path_fix(os.path.join(result_path, scene, item, pos_name + ".json")),
+                    path_fix(
+                        os.path.join(result_path, scene, item, pos_name + ".json")
+                    ),
                     "w",
                     encoding="utf-8",
                 ) as f:
@@ -153,12 +131,104 @@ def item_point_generate(language_id):
                     )
                     # f.write(lines)
                     json.dump(pos_json, f)
-# print(count_dict)
+
+
+def json_pos_generate_dict(
+    language_id,
+    id_json,
+    item_json,
+    result_path,
+    pos_type_name="gatherItemId",
+    id_name_json_filename="Item.json",
+):
+    global count_dict
+    item_dict = {}
+    for scene_data in item_json:
+        # print(scene_data["sceneId"])
+        scene_data["sceneId"] = str(scene_data["sceneId"])
+        scene_data["sceneId"] = id_json["Scene.json"][scene_data["sceneId"]][
+            language_id
+        ]
+        if scene_data["sceneId"] not in item_dict.keys():
+            item_dict.update({scene_data["sceneId"]: {}})
+        new_folder(os.path.join(result_path, scene_data["sceneId"]))
+        for item in scene_data["spawns"]:
+            if pos_type_name in item:
+                item[pos_type_name] = str(item[pos_type_name])
+                if item[pos_type_name] in id_json[id_name_json_filename].keys():
+                    item[pos_type_name] = id_json[id_name_json_filename][
+                        item[pos_type_name]
+                    ][language_id]
+                if item[pos_type_name] not in item_dict[scene_data["sceneId"]].keys():
+                    item_dict[scene_data["sceneId"]].update({item[pos_type_name]: []})
+                new_folder(
+                    os.path.join(
+                        result_path, scene_data["sceneId"], item[pos_type_name]
+                    )
+                )
+                pos = [item["pos"]["x"], item["pos"]["y"], item["pos"]["z"]]
+                item_dict[scene_data["sceneId"]][item[pos_type_name]].append(pos)
+                count(item[pos_type_name])
+        # count(scene_data["sceneId"])
+    # print(item_dict)
+    count_dict_sort = sorted(
+        count_dict.items(), key=lambda x: x[1], reverse=True
+    )  # 按照value排序
+    print(count_dict_sort)
+    count_dict = {}
+    return item_dict
+
+
+def get_pos_json(item_json_name):
+    item_json_path = os.path.join(os.path.dirname(__file__), "pos", item_json_name)
+    with open(item_json_path, "r") as f:
+        item_json = json.load(f)
+    return item_json
+
+
+def main___item_point_generate(language_id):
+    # language_id = 3  # 0:cn,1:tw,2:en,3:ru
+    folader_name = os.path.join(os.path.dirname(__file__), "ID_json")
+    id_json = get_all_id_name(folader_name)
+    result_path = os.path.join(
+        os.path.dirname(__file__),
+        "result",
+        language_name[language_id],
+    )
+    #######################################################################
+    item_json = get_pos_json(item_json_name)
+    item_result_path = os.path.join(result_path, "Item")
+    item_dict = json_pos_generate_dict(
+        language_id,
+        id_json,
+        item_json,
+        item_result_path,
+        pos_type_name="gatherItemId",
+        id_name_json_filename="Item.json",
+    )
+    save_json(item_result_path, item_dict)
+    ################################################################
+    monster_json = get_pos_json(monster_json_name)
+    monster_result_path = os.path.join(result_path, "Monster_And_Animal")
+    for k,v in id_json["Monster.json"].items():
+        if k in id_json["Animal.json"].keys():
+            print(k,v,"-------------------------------------------------------------------------")
+    id_json["Monster.json"].update(id_json["Animal.json"])
+    monster_dict = json_pos_generate_dict(
+        language_id,
+        id_json,
+        monster_json,
+        monster_result_path,
+        pos_type_name="monsterId",
+        id_name_json_filename="Monster.json",
+    )
+    save_json(monster_result_path, monster_dict)
+
 
 if __name__ == "__main__":
     # for i in range(4):
     #     item_point_generate(i)
-    item_point_generate(0)
-    item_point_generate(1)
-    item_point_generate(2)
-    item_point_generate(3)
+    main___item_point_generate(0)
+    # main___item_point_generate(1)
+    # main___item_point_generate(2)
+    # main___item_point_generate(3)
